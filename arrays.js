669f2d526b0f7720e4309b23a2613cf0766ad864
Jenn <thai.jenn@gmail.com>
2017-01-31 13:05:00 -0800

    arrays

---
+++ arrays.js
@@ -0,0 +1,35 @@
+
+
+// a.forEach(function(element){
+//   console.log(element);
+// });
+
+
+var a = [0.1,0.01,0.001];
+var max = a[0];
+
+function highNum() {
+  a.forEach(function(element){
+  if (element < max) {
+    max = element;
+    }
+  });
+  return max
+};
+
+
+// function highestNum() {
+//   a.sort(function(element1, element2) {
+//     return element1-element2;
+//   });
+//   console.log([a.length-1]);
+// }
+
+var sum =[1,2,3] ;
+
+function findSum() {
+
+
+  })
+
+}
